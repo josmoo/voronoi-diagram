a756cc17eed25b8118994b002c62eea6a5a594e1
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-10-12 17:51:45 -0500

    init

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -xe
+
+cc -Wall -Wextra -o voronoi ./src/main.c
---
+++ src/main.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+#define SEEDS_COUNT 10
+#define SEED_MARKER_RADIUS 10
+#define SEED_MARKER_COLOR 0xFF000000
+
+#define OUTPUT_FILE_PATH "output.ppm"
+
+typedef struct {
+    int x, y;
+} Point;
+
+static uint32_t image[HEIGHT][WIDTH];
+static Point seeds[SEEDS_COUNT];
+ 
+void generate_random_seeds(void){
+    srand((unsigned int)time(NULL));
+    for (size_t i = 0; i < SEEDS_COUNT; ++i){
+        seeds[i].x = rand() % WIDTH;
+        seeds[i].y = rand() % HEIGHT;
+    }
+}
+int sqr_dist(int x1, int y1, int x2, int y2){
+    int dx = x1 - x2;
+    int dy = y1 - y2;
+    return dx*dx + dy*dy;
+}
+
+void fill_circle(int cx, int cy, int radius, uint32_t color){
+    Point start = {cx - radius, cy - radius};
+    Point end = {cx + radius, cy + radius};
+
+    for(int x = start.x; x < end.x; ++x){
+        for (int y = start.y; y < end.y; ++y){
+            if(x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT){
+                continue;
+            }
+            if(sqr_dist(cx, cy, x, y) <= radius*radius){
+                image[y][x] = color;
+            }
+            
+        }
+    }
+}
+
+void render_seed_markers(void){
+    for(size_t i = 0; i < SEEDS_COUNT; ++i){
+        fill_circle(seeds[i].x, seeds[i].y, SEED_MARKER_RADIUS, SEED_MARKER_COLOR);
+    }
+}
+
+// void render_voronoi(void){
+//     for(int y = 0; y < HEIGHT; ++y){
+//         for(int x = 0; x < WIDTH; ++x){
+//             int j = 0;
+//             for (size_t i = 1; i < SEEDS_COUNT; ++i){
+//                 sqr_dist(seeds[i].x, seeds[i].y, x, y);
+//                 sqr_dist(seeds[j].x, seeds[j].y, x, y);
+//             }
+//         }
+//     }
+// }
+
+
+void fill_image(uint32_t color){
+    for(size_t y = 0; y < HEIGHT; ++y){
+        for(size_t x = 0; x < WIDTH; ++x){
+            image[y][x] = color;
+        }
+    }
+}
+
+
+void save_image_as_ppm(const char *file_path){
+    FILE *f = fopen(file_path, "wb");
+    if(f == NULL){
+        fprintf(stderr, "ERROR: could not write into file %s.\n%s\n", file_path, strerror(errno));
+    }
+
+    fprintf(f, "P6\n%d %d 255\n", WIDTH, HEIGHT);
+    for(size_t y = 0; y < HEIGHT; ++y){
+        for(size_t x = 0; x < WIDTH; ++x){
+            uint32_t pixel = image[y][x];
+
+            //little endian
+            uint8_t bytes[3] = { 
+                (pixel & 0x000000FF),    //R
+                (pixel & 0x0000FF00)>>8, //G
+                (pixel & 0x00FF0000)>>16 //B
+            };
+            fwrite(bytes, sizeof(bytes), 1, f);
+            assert(!ferror(f));
+        }
+    }
+    assert(fclose(f) == 0);
+}
+
+int main(void){
+    generate_random_seeds();
+    fill_image(0xFFFFDD00); 
+    render_seed_markers();
+    save_image_as_ppm(OUTPUT_FILE_PATH);
+    printf("hello world");
+    return 0;
+}
