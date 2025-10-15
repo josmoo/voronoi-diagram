0f5ede22b2fd051ad13e0820a7738db8cd316b31
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-10-15 12:41:00 -0500

    corrected the JFA in render_voronoi

--- src/main.c
+++ src/main.c
@@ -6,11 +6,9 @@
 #include <errno.h>
 #include <time.h>
 
-#define WIDTH 800
-#define HEIGHT 800
+#define WIDTH_AND_HEIGHT 600
 
-#define SEEDS_COUNT 32
-#define SEED_MARKER_COLOR 0xFF000000
+#define SEEDS_COUNT 256
 #define UNDEFINED_COLOR 0x00BABABA
 
 #define EMPTY_ORIGIN (Point){-1,-1}
@@ -26,14 +24,14 @@ typedef struct {
     Point origin;
 } Pixel;
 
-static Pixel image[HEIGHT][WIDTH];
+static Pixel image[WIDTH_AND_HEIGHT][WIDTH_AND_HEIGHT];
 static Point seeds[SEEDS_COUNT];
  
 void generate_random_seeds(void){
     srand((unsigned int)time(NULL));
     for (size_t i = 0; i < SEEDS_COUNT; ++i){
-        seeds[i].x = rand() % WIDTH;
-        seeds[i].y = rand() % HEIGHT;
+        seeds[i].x = rand() % WIDTH_AND_HEIGHT;
+        seeds[i].y = rand() % WIDTH_AND_HEIGHT;
     }
 }
 int sqr_dist(int x1, int y1, int x2, int y2){
@@ -56,46 +54,51 @@ void render_seed_markers(void){
     }
 }
 
+//https://en.wikipedia.org/wiki/Jump_flooding_algorithm#Implementation
 void render_voronoi(void){
-    int k = WIDTH/2;
-    while(k > 2){
-        int offset = k/2;
-        for(int y = offset-1; y < HEIGHT - offset; ++y){
-            for(int x = offset; x < WIDTH - offset; ++x){
-                Point neighbors[8] = {
-                    {x - offset, y - offset}, {x, y - offset}, {x + offset, y - offset},
-                    {x - offset, y},                           {x + offset, y},
-                    {x - offset, y + offset}, {x, y + offset}, {x + offset, y + offset} 
-                };
-                for(int i = 0; i < 8; ++i){
-                    uint32_t neighbors_color = image[neighbors[i].y][neighbors[i].x].color;
-                    if(neighbors_color == UNDEFINED_COLOR){
-                       continue;
-                    }
-
-                    Point neighbors_origin = image[neighbors[i].y][neighbors[i].x].origin;
-                    if(image[y][x].color == UNDEFINED_COLOR){
-                        image[y][x].color = neighbors_color;
-                        image[y][x].origin = neighbors_origin;
+    for(int k = WIDTH_AND_HEIGHT/2; k > 0; k/=2){
+        printf("%d ",k);
+        for (int y = 0; y < WIDTH_AND_HEIGHT; ++y){
+            for (int x = 0; x < WIDTH_AND_HEIGHT; ++x){
+                for(int i = y - k; i <= k + y; i += k){
+                    if(i < 0 || i >= WIDTH_AND_HEIGHT){
                         continue;
                     }
-                    int sqr_dist_to_pixel_origin = sqr_dist(x, y, image[y][x].origin.x, image[y][x].origin.y);
-                    int sqr_dist_to_neighbor_origin = sqr_dist(x, y, neighbors_origin.x, neighbors_origin.y);
-                    if(sqr_dist_to_neighbor_origin < sqr_dist_to_pixel_origin){
-                       image[y][x].color = neighbors_color;
-                       image[y][x].origin = neighbors_origin;
-                       continue;
+                    for(int j = x - k; j <= k + x; j += k){
+                        if(j < 0 || j >= WIDTH_AND_HEIGHT){
+                            continue;
+                        }
+                        if(i == y && j == x){
+                            continue;
+                        }
+                        uint32_t neighbors_color = image[i][j].color;
+                        if(neighbors_color == UNDEFINED_COLOR){
+                            continue;
+                        }
+                        Point neighbors_origin = image[i][j].origin;
+                        if(image[y][x].color == UNDEFINED_COLOR){
+                            image[y][x].color = neighbors_color;
+                            image[y][x].origin = neighbors_origin;
+                            continue;
+                        }
+
+                        int sqr_dist_to_pixel_origin = sqr_dist(x, y, image[y][x].origin.x, image[y][x].origin.y);
+                        int sqr_dist_to_neighbor_origin = sqr_dist(x, y, neighbors_origin.x, neighbors_origin.y);
+                        if(sqr_dist_to_neighbor_origin < sqr_dist_to_pixel_origin){
+                            image[y][x].color = neighbors_color;
+                            image[y][x].origin = neighbors_origin;
+                            continue;
+                        }
                     }
                 }
             }
         }
-        k = offset;
     }
 }
 
 void fill_image(uint32_t color){
-    for(size_t y = 0; y < HEIGHT; ++y){
-        for(size_t x = 0; x < WIDTH; ++x){
+    for(size_t y = 0; y < WIDTH_AND_HEIGHT; ++y){
+        for(size_t x = 0; x < WIDTH_AND_HEIGHT; ++x){
             image[y][x] = (Pixel){color, EMPTY_ORIGIN};
         }
     }
@@ -107,9 +110,9 @@ void save_image_as_ppm(const char *file_path){
         fprintf(stderr, "ERROR: could not write into file %s.\n%s\n", file_path, strerror(errno));
     }
 
-    fprintf(f, "P6\n%d %d 255\n", WIDTH, HEIGHT);
-    for(size_t y = 0; y < HEIGHT; ++y){
-        for(size_t x = 0; x < WIDTH; ++x){
+    fprintf(f, "P6\n%d %d 255\n", WIDTH_AND_HEIGHT, WIDTH_AND_HEIGHT);
+    for(size_t y = 0; y < WIDTH_AND_HEIGHT; ++y){
+        for(size_t x = 0; x < WIDTH_AND_HEIGHT; ++x){
             Pixel pixel = image[y][x];
 
             //little endian
@@ -131,6 +134,5 @@ int main(void){
     render_seed_markers();
     render_voronoi();
     save_image_as_ppm(OUTPUT_FILE_PATH);
-    printf("hello world");
     return 0;
 }
